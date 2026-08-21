509b0490697c9b3030e50611f109dbd42bcb9c84
Wai Hlyan Min Thein <waihlyanminthein@gmail.com>
2026-08-20 23:32:20 -0400

    Time: 228 ms (85.57%) | Memory: 19.4 MB (18.13%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/longest-palindromic-substring.py
@@ -1,19 +1,23 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        n = len(s)
-        dp = [[True] * n for _ in range(n)]
+        if not s:
+            return ""
 
-        start_pos = 0
-        max_length = 1
+        start, max_len = 0, 0
 
-        for i in range(n - 2, -1, -1):
-            for j in range(i + 1, n):
-                dp[i][j] = False
+        for i in range(len(s)):
+            len1 = self.expand_from_center(s, i, i)
+            len2 = self.expand_from_center(s, i, i + 1)
 
-                if s[i] == s[j]:
-                    dp[i][j] = dp[i + 1][j - 1]
+            current_len = max(len1, len2)
+            if current_len > max_len:
+                max_len = current_len
+                start = i - (current_len - 1) // 2
 
-                    if dp[i][j] and max_length < j - i + 1:
-                        start_pos = i
-                        max_length = j - i + 1
-        return s[start_pos : start_pos + max_length]
+        return s[start : start + max_len]
+
+    def expand_from_center(self, s: str, left: int, right: int) -> int:
+        while left >= 0 and right < len(s) and s[left] == s[right]:
+            left -= 1
+            right += 1
+        return right - left - 1
